eba8ce59d7ae8550f4fb7efb7a9e68f77725fc5b
MasaoFujii <masao.fujii@gmail.com>
2015-02-20 22:07:15 +0900

    Remove unused hook and related function.

--- pg_audit_trail.c
+++ pg_audit_trail.c
@@ -21,7 +21,6 @@ static int	nested_level = 0;
 static ExecutorStart_hook_type prev_ExecutorStart = NULL;
 static ExecutorRun_hook_type prev_ExecutorRun = NULL;
 static ExecutorFinish_hook_type prev_ExecutorFinish = NULL;
-static ExecutorEnd_hook_type prev_ExecutorEnd = NULL;
 static ProcessUtility_hook_type prev_ProcessUtility = NULL;
 
 static void pgat_ExecutorStart(QueryDesc *queryDesc, int eflags);
@@ -29,7 +28,6 @@ static void pgat_ExecutorRun(QueryDesc *queryDesc,
 				 ScanDirection direction,
 				 long count);
 static void pgat_ExecutorFinish(QueryDesc *queryDesc);
-static void pgat_ExecutorEnd(QueryDesc *queryDesc);
 static void pgat_ProcessUtility(Node *parsetree, const char *queryString,
 					ProcessUtilityContext context, ParamListInfo params,
 					DestReceiver *dest, char *completionTag);
@@ -63,8 +61,6 @@ _PG_init(void)
 	ExecutorRun_hook = pgat_ExecutorRun;
 	prev_ExecutorFinish = ExecutorFinish_hook;
 	ExecutorFinish_hook = pgat_ExecutorFinish;
-	prev_ExecutorEnd = ExecutorEnd_hook;
-	ExecutorEnd_hook = pgat_ExecutorEnd;
 	prev_ProcessUtility = ProcessUtility_hook;
 	ProcessUtility_hook = pgat_ProcessUtility;
 }
@@ -79,7 +75,6 @@ _PG_fini(void)
 	ExecutorStart_hook = prev_ExecutorStart;
 	ExecutorRun_hook = prev_ExecutorRun;
 	ExecutorFinish_hook = prev_ExecutorFinish;
-	ExecutorEnd_hook = prev_ExecutorEnd;
 	ProcessUtility_hook = prev_ProcessUtility;
 }
 
@@ -137,15 +132,6 @@ pgat_ExecutorFinish(QueryDesc *queryDesc)
 		PG_RE_THROW();
 	}
 	PG_END_TRY();
-}
-
-static void
-pgat_ExecutorEnd(QueryDesc *queryDesc)
-{
-	if (prev_ExecutorEnd)
-		prev_ExecutorEnd(queryDesc);
-	else
-		standard_ExecutorEnd(queryDesc);
 }
 
 static void
